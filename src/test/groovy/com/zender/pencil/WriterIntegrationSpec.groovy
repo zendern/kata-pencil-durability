c5282709383462bd16be9c54d7aac35c67c07e43
Nathan Zender <nzender1986@gmail.com>
2018-06-18 23:49:07 -0400

    Figured an integration test was in order.
    Just wanted that happy fuzzy that the whole system was wired up together
    correctly. So testing from the Person/Writer perspective happy path all
    the way through.
    
    Write -> Read -> Sharpen -> Write Some More -> Read some More -> Erase
    -> Final Read

---
+++ src/test/groovy/com/zender/pencil/WriterIntegrationSpec.groovy
@@ -0,0 +1,45 @@
+package com.zender.pencil
+
+import com.zender.pencil.surfaces.Paper
+import com.zender.pencil.surfaces.WritingSurface
+import com.zender.pencil.utensils.Pencil
+import com.zender.pencil.utensils.PencilSharpener
+import com.zender.pencil.utensils.UtensilSharpener
+import com.zender.pencil.utensils.WritingUtensil
+import spock.lang.Specification
+
+class WriterIntegrationSpec extends Specification {
+
+
+    def "happy path"(){
+        given : "I have all my tools"
+            WritingUtensil pencil = new Pencil(16)
+            WritingSurface surface = new Paper()
+            UtensilSharpener sharpener = new PencilSharpener()
+
+        and :
+            Person writer = new Person(writingUtensil: pencil, writingSurface: surface, sharpener: sharpener)
+
+        when : "I want to write something down"
+            writer.write("big gulps huh well alright then")
+
+        then : "I can read what I wrote"
+            writer.read() == "big gulps huh well a"
+
+        and : "I did not finish my thought b/c my pencil ran out so i sharpen it"
+            writer.sharpenUtensil()
+
+        and : "then I write some more"
+            writer.write("lright then bye")
+
+        and : "then I read that thought"
+            writer.read() == "big gulps huh well alright then bye"
+
+        and : "then I erase that last part"
+            writer.erase("bye")
+
+        and : "and proof read one more time"
+            writer.read() == "big gulps huh well alright then    "
+    }
+
+}
